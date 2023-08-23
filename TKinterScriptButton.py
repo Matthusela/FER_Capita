0d531dc6879dbac2b65093f9f8a1dbe2594b1a5e
Matt Warren <89740948+Matthusela@users.noreply.github.com>
2023-08-23 14:45:37 +0100

    Update TKinterScriptButton.py

--- TKinterScriptButton.py
+++ TKinterScriptButton.py
@@ -4,33 +4,29 @@ Created on Tue Jul  4 10:25:12 2023
 
 @author: matt8
 """
+# External Modules
 import tkinter as tk
 from PIL import ImageTk, Image
 import cv2
-import sys
-import pandas as pd
-import pickle
-import tensorflow as tf
-import matplotlib.pyplot as plt
-import numpy as np
-#from Model1 import emotion_dict
 import os
-#from dataset_cleaning import number_of_images, image_dimension
-#from Model1 import random_sample, gs_to_rgb
-#os.chdir("FER_Capita")
-print(os.getcwd())
-#df = pd.read_pickle("df_1.pkl")
+if os.getcwd() != 'C:\\Users\\matt8\\fer_capita':
+    os.chdir('C:\\Users\\matt8\\fer_capita')
+import tensorflow as tf
+import numpy as np
 
-with open("emo_dict.pkl", "rb") as file:
-    emotion_dict = pickle.load(file)
+# Project files
+from Model1 import emotion_dict
+from dataset_cleaning import inp_dim
 
-IMDIM = 48
+IMDIM = inp_dim
 NUMCLASS = len(emotion_dict)
-# Access the boolean values
-RGB_or_GS = True
+# Access the boolean values - these 2 lines as talked about in the ReadMe file are currently unnecessary and were introduced to 
+# easily retrieve specific models in conjuction with directory naming convention from Model_Files() of RandomModels file.
+# Do not think that code below is set up for models trained on RGB rather than GreyScale images.
+RGB_or_GS = False
 OneHot = True
-#dir_name = "ModelsRGB{}OneHot{}".format(str(RGB_or_GS), str(OneHot))
-dir_name = "TrainedModelsGS"
+# The dir_name variable reflects where the trained expression models have been saved - likely to be necessary to change this line
+dir_name = "AugmentModels"
 print(dir_name)
 print(emotion_dict)
 model_files = os.listdir(dir_name)
@@ -38,13 +34,15 @@ loaded_models = {}
 for file_name in model_files:
     # Construct the full path to the model file
     model_path = os.path.join(dir_name, file_name)
-
     # Load the model
     model = tf.keras.models.load_model(model_path)
-
     # Add the loaded model to the dictionary using the file name as the key
     loaded_models[file_name] = model
-print(loaded_models)
+    
+print(list(loaded_models.keys()))
+
+# This is the CV2 algorithm that has been introduced to identify faces in images (see 23/08/23 note in ReadMe)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 
 def get_emo(image_pixels, dict_of_models):
@@ -67,19 +65,34 @@ def start_detection():
     cap = cv2.VideoCapture(0)
 
     def process_frame():
-        global IMDIM
+        global IMDIM, face_cascade
         # Read frame from the camera and get the matrix of pixel values
         _, frame = cap.read()
+        
         # Convert pixel matrix to grayscale if the model is grayscale-based
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         display_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # Reduce image size to match the dimensions of the model's reference images
-        resized_frame = cv2.resize(gray_frame, (IMDIM, IMDIM))
-        # Normalize pixel values to [0,1] interval
-        norm_frame = resized_frame / 255
-        pixels = norm_frame.reshape(1, IMDIM, IMDIM, 1)
-        # Make predictions using the facial model
-        emotion = get_emo(pixels, loaded_models)
+        
+        # The faces uses a pre-trained facial detection model to identify a face
+        # in the image. The face area is then padded out slightly in order to match
+        # the characteristic padding of the training images
+        faces = face_cascade.detectMultiScale(display_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        for (x, y, w, h) in faces:
+            pad = 40 
+            cv2.rectangle(display_frame, (x-pad, y-pad),
+                          (x + w+pad, y + h+pad), (255, 0, 0), 2)
+        
+        # If the face_cascade model has detected a face, then use the bounded region,
+        # shown by the rectangle in the live feed, and perform the emotion detection
+        # on just this region.
+        if len(faces) > 0:
+            image_frame = gray_frame[y-pad:y+h+pad, x-pad:x+w+pad]
+            calibrated_image_frame = cv2.resize(image_frame, (IMDIM, IMDIM)) / 255.0
+            pixels = calibrated_image_frame.reshape(1, IMDIM, IMDIM, 1)
+            emotion = get_emo(pixels, loaded_models)
+            
+        else:
+            emotion = "No face present"
 
         # Convert the frame to ImageTk format for displaying in the Tkinter window
         image = Image.fromarray(display_frame)
@@ -96,6 +109,10 @@ def start_detection():
         # Update the emotion label
         emotion_label.config(text=emotion)
         window.after(10, process_frame)
+        
+        
+        
+        
     # Start processing the frames
     window.after(0, process_frame)
 
@@ -108,8 +125,8 @@ def start_detection_with_button():
 
 # Create the Tkinter window
 window = tk.Tk()
-window.title("Live Face Recognition")
-window.geometry("300x480")
+window.title("Live Expression Recognition")
+window.geometry("200x200")
 
 # ... your existing code for labels and other UI elements ...
 frame = tk.Frame(window)
